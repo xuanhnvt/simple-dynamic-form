7e1cf52ebf4e4d382e104b43406cda6595ef2604
Hung Xuan <xuanhn.vt@gmail.com>
2019-03-28 16:05:12 +0700

    Setup to run with basic angular material component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,59 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {
+  MatListModule,
+  MatButtonModule,
+  MatInputModule,
+  MatIconModule,
+  MatChipsModule,
+  MatToolbarModule,
+  MatFormFieldModule,
+  MatSidenavModule,
+  MatTabsModule,
+  MatSliderModule,
+  MatProgressBarModule,
+  MatStepperModule,
+  MatExpansionModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatCheckboxModule,
+  MatTooltipModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatGridListModule
+} from '@angular/material';
+
+const MAT_MODULES = [MatListModule,
+  MatButtonModule,
+  MatInputModule,
+  MatIconModule,
+  MatChipsModule,
+  MatToolbarModule,
+  MatFormFieldModule,
+  MatSidenavModule,
+  MatTabsModule,
+  MatSliderModule,
+  MatProgressBarModule,
+  MatStepperModule,
+  MatExpansionModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatCheckboxModule,
+  MatTooltipModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatGridListModule
+];
 
 @NgModule({
   declarations: [
@@ -10,6 +61,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    [...MAT_MODULES],
     AppRoutingModule
   ],
   providers: [],
